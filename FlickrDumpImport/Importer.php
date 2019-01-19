34fa273758148c08504f4c2ddc1e649fdc30b2dd
Marcus Povey <marcus@marcus-povey.co.uk>
2019-01-19 19:47:35 +0000

    Importing video outline

--- FlickrDumpImport/Importer.php
+++ FlickrDumpImport/Importer.php
@@ -115,41 +115,80 @@ class Importer {
                                     
                                     \Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('Importing %s as a photo', [$file]));
         
-                                    $_FILES = [
-                                        'photo' => [
-                                            'tmp_name' => $file,
-                                            'name' => basename($json['original']),
-                                            'type' => $mime,
-                                        ]
-                                    ];
-                                    
-                                    $tags = [];
-                                    foreach ($json['tags'] as $tag) {
-                                        $tags[] = '#' . trim(str_replace(' ', '', $tag['tag']) , '"#\'');
-                                    }
-                                    
-                                    \Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('\tSetting title as %s', [$json['name']]));
-                                    \Idno\Core\Idno::site()->currentPage()->setInput('title', $json['name']);
-                               
-                                    \Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('\tSetting body as %s', [empty($tags) ? $json['description'] : $json['description'] . "\n\n" . implode(' ', $tags)]));
-                                    \Idno\Core\Idno::site()->currentPage()->setInput('body', empty($tags) ? $json['description'] : $json['description'] . "\n\n" . implode(' ', $tags));
-               
-                                    \Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('\tSetting created time to %s', [$json['date_taken']]));
-                                    \Idno\Core\Idno::site()->currentPage()->setInput('created', strtotime($json['date_taken']));
-                                    
-                                    
-                                    
+				    $photo_obj = \IdnoPlugins\Photo\Photo::getOneFromAll(array('flickr_id' => $photo['id']));
+				    if (!$photo_obj) {
+					
+					$photo_obj = new \IdnoPlugins\Photo\Photo();
+					$photo_obj->flickr_id = $photo['id'];
+					
+					$_FILES = [
+					    'photo' => [
+						'tmp_name' => $file,
+						'name' => basename($json['original']),
+						'type' => $mime,
+					    ]
+					];
+				    } else {
+					
+					\Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('Photo has already been imported, so amending values'));
+				    }
+
                                     break;
                                 
                                 case 'mp4' :
                                     $mime = 'video/mp4';
                                     \Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('Importing %s as a video', [$file]));
+				    
+				    $photo_obj = \IdnoPlugins\Media\Media::getOneFromAll(array('flickr_id' => $photo['id']));
+				    if (!$photo_obj) {
+					$photo_obj = new \IdnoPlugins\Media\Media();
+					$photo_obj->flickr_id = $photo['id'];
+					
+					$_FILES = [
+					    'photo' => [
+						'tmp_name' => $file,
+						'name' => basename($json['original']),
+						'type' => $mime,
+					    ]
+					];
+				    } else {
+					\Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('Video has already been imported, so amending values'));
+				    }
+				    
                                     break;
                                 
                                 default:
                                     throw new \RuntimeException(\Idno\Core\Idno::site()->language()->_('Extension %s is not supported for %s', [$ext, $file]));
                             }
                             
+			    
+			    $tags = [];
+			    foreach ($json['tags'] as $tag) {
+				$tags[] = '#' . trim(str_replace(' ', '', $tag['tag']) , '"#\'');
+			    }
+
+			    \Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('\tSetting title as %s', [$json['name']]));
+			    \Idno\Core\Idno::site()->currentPage()->setInput('title', $json['name']);
+
+			    \Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('\tSetting body as %s', [empty($tags) ? $json['description'] : $json['description'] . "\n\n" . implode(' ', $tags)]));
+			    \Idno\Core\Idno::site()->currentPage()->setInput('body', empty($tags) ? $json['description'] : $json['description'] . "\n\n" . implode(' ', $tags));
+
+			    \Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('\tSetting created time to %s', [$json['date_taken']]));
+			    \Idno\Core\Idno::site()->currentPage()->setInput('created', strtotime($json['date_taken']));
+			    
+			    if (!empty($json['geo'])) {
+				\Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('\tSetting lat/long as %s/%s', [$json['geo']['latitude'], $json['geo']['longitude']]));
+				$photo_obj->lat = $json['geo']['latitude'];
+				$photo_obj->ong = $json['geo']['longitude'];
+			    }
+
+			    $photo_obj->flickr_page = $json['photopage'];
+			    \Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('\tPhoto page saved as %s', [$json['photopage']]));
+
+			    if ($photo_obj->saveDataFromInput())
+				\Idno\Core\Idno::site()->logging()->info(\Idno\Core\Idno::site()->language()->_('Photo saved as %s', [$photo_obj->getUrl()]));
+
+			    $photo_obj = null;
                             
                         } else {
                             throw new \RuntimeException(\Idno\Core\Idno::site()->language()->_('Could not find a file associated with %s', [$json['id']]));
